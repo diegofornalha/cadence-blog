afd5ce5284ee2a372b35b24d2a645a6310d53bb3
Stackbit Code Editor <projects@stackbit.com>
2023-01-01 06:36:46 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -19,7 +19,7 @@ export default function MyApp({ Component, pageProps }) {
     return () => {
       router.events.off("routeChangeComplete", handleRouteChange);
     };
-  }, [router.events]);
+    }, [router.events]);
 
 
     return <Component {...pageProps} />;
