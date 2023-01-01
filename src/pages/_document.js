deb77eae122b979dd368e9dbd8db678bb0383bce
Stackbit Code Editor <projects@stackbit.com>
2023-01-01 08:16:42 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,7 +1,7 @@
 import Analytics from '../components/Analytics'
 
 import Document, { Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import '../css/main.css';
 
 export default class MyDocument extends Document {
     static async getInitialProps(ctx) {
