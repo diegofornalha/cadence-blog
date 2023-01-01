0e1d7e65568e6fa33737bf55a6d8cce095996b7b
Stackbit Code Editor <projects@stackbit.com>
2023-01-01 07:38:28 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -22,7 +22,7 @@ export default function MyApp({ Component, pageProps }) {
     return (
         <>
             <Script
-            async
+                async
                 src={`https://www.googletagmanager.com/gtag/js?id=G-QH6FTZ9VEL4`}
                 strategy="afterInteractive"
             />
