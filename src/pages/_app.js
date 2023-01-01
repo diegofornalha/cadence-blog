a2f922a7367072a03ce1ffec6637cfbe49172818
Stackbit Code Editor <projects@stackbit.com>
2023-01-01 06:35:19 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,24 @@ import { pageview } from "../lib/ga";
 
 
 export default function MyApp({ Component, pageProps }) {
+    const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url);
+    };
+
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
 
     return <Component {...pageProps} />;
 
