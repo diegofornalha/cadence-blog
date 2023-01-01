2698a528a82e414805ac900abd097b89fcbf6f71
Stackbit Code Editor <projects@stackbit.com>
2023-01-01 07:03:52 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,19 +3,43 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Script from 'next/script';
 
+import * as gtag from '../lib/gtag';
+
 export default function MyApp({ Component, pageProps }) {
-    <>
 
-        <Script src={'https://www.googletagmanager.com/gtag/js?id=G-99JF8KBFVZ'} strategy='afterInteractive' />
-        <Script id="google-analytics" strategy="afterInteractive">
-            {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-99JF8KBFVZ');
-        `}
-        </Script>
-    </>
-
-    return <Component {...pageProps} />;
+    const router = useRouter();
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            window.gtag('config', 'G-99JF8KBFVZ', {
+                page_path: url,
+            });
+        };
+        router.events.on('routeChangeComplete', handleRouteChange);
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+        };
+    }, [router.events])
+    return (
+        <>
+            <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=G-99JF8KBFVZ`}
+                strategy="afterInteractive"
+            />
+            <Script
+                id="gtag-init"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-99JF8KBFVZ', {
+                            page_path: window.location.pathname,
+                        });
+                    `,
+                }}
+            />
+            <Component {...pageProps} />
+        </>
+    );
 }
