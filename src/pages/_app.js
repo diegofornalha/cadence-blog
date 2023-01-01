466649e147bd7f364150f4e13ce15be566e9a28f
Stackbit Code Editor <projects@stackbit.com>
2023-01-01 02:11:09 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,7 +12,7 @@ export default function MyApp({ Component, pageProps }) {
     gtag('config', 'G-VWPJJRMJCP');
     `}
         </Script>
-            </>;
+    </>;
         return <Component {...pageProps} />;
 
 }
