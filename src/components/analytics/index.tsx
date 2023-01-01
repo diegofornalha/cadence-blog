10f86d625f9de0fde93f648d3498d09aa0c88ddc
Stackbit Code Editor <projects@stackbit.com>
2023-01-01 07:48:47 +0000

    index.tsx: changed

--- src/components/analytics/index.tsx
+++ src/components/analytics/index.tsx
@@ -1,24 +1,5 @@
 import React from 'react'
 
 const Analytics = () => (
-            <>
-            <Script
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=G-QH6FTZ9VEL4`}
 
-            />
-            <Script
-                dangerouslySetInnerHTML={{
-                    __html: `
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-                        gtag('config', 'G-QH6FTZ9VEL', {
-                            page_path: window.location.pathname,
-                        });
-                    `,
-                }}
-            />
-        </>
-)
 export default Analytics
