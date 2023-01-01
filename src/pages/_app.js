f6eda815b7058149ad7191726274332c647c4757
Stackbit Code Editor <projects@stackbit.com>
2023-01-01 07:46:32 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -20,24 +20,7 @@ export default function MyApp({ Component, pageProps }) {
         };
     }, [router.events])
     return (
-        <>
-            <Script
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=G-QH6FTZ9VEL4`}
 
-            />
-            <Script
-                dangerouslySetInnerHTML={{
-                    __html: `
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-                        gtag('config', 'G-QH6FTZ9VEL', {
-                            page_path: window.location.pathname,
-                        });
-                    `,
-                }}
-            />
             <Component {...pageProps} />
         </>
     );
