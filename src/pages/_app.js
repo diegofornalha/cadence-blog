98248104bcf7de9e885532a417f5333b0c788896
Stackbit Code Editor <projects@stackbit.com>
2023-01-01 07:58:32 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,24 +1,5 @@
 import '../css/main.css';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import * as gtag from '../lib/gtag';
 
 export default function MyApp({ Component, pageProps }) {
-
-    const router = useRouter();
-    useEffect(() => {
-        const handleRouteChange = (url) => {
-            window.gtag('config', 'G-QH6FTZ9VEL', {
-                page_path: url,
-            });
-        };
-        router.events.on('routeChangeComplete', handleRouteChange);
-        return () => {
-            router.events.off('routeChangeComplete', handleRouteChange);
-        };
-    }, [router.events])
-    return (
-
-            <Component {...pageProps} />
-    );
+    return <Component {...pageProps} />;
 }
