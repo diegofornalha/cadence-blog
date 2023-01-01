d8b588480b7efdc1712634834ae9905ec9721687
Stackbit Code Editor <projects@stackbit.com>
2023-01-01 08:17:10 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,34 +1,8 @@
 import Analytics from '../components/Analytics'
 
 import Document, { Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-    static async getInitialProps(ctx) {
-        const sheet = new ServerStyleSheet()
-        const originalRenderPage = ctx.renderPage
-
-        try {
-            ctx.renderPage = () =>
-                originalRenderPage({
-                    enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
-                })
-
-                const initialProps = await Document.getInitialProps(ctx)
-                return {
-                    ...initialProps,
-                    styles: (
-                        <>
-                            {initialProps.styles}
-                            {sheet.getStyleElement()}
-                        </>
-                    ),
-                }
-        } finally {
-            sheet.seal()
-        }   
-    }
-    
     render() {
         return (
             <html>
@@ -37,6 +11,7 @@ export default class MyDocument extends Document {
                 <body>
                     <Main />
                     <NextScript />
+                    <Analytics />
                 </body>
             </html>
         );
