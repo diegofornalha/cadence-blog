fe13d56094a47c62cc31930f0cbb5f96199f3249
Stackbit Code Editor <projects@stackbit.com>
2023-01-01 08:09:45 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,6 +11,7 @@ export default class MyDocument extends Document {
                 <body>
                     <Main />
                     <NextScript />
+                              <Analytics />
                 </body>
             </html>
         );
