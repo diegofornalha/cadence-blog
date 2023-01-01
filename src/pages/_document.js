ee6375bd1689c646679c45d6f03eab22065d6ef6
Stackbit Code Editor <projects@stackbit.com>
2023-01-01 08:06:23 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,12 +1,17 @@
-import Document, { Main, NextScript } from 'next/document'
+
+
+import Document, { Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
     render() {
         return (
             <html>
+                <Head>
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
+                    <Analytics />
 
                 </body>
             </html>
