ab69bcfa2d75566ff09aea50ea23acbfba0b505a
Stackbit Code Editor <projects@stackbit.com>
2023-01-01 06:34:14 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
 
 import '../css/main.css';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { pageview } from "../lib/ga";
+
 
 export default function MyApp({ Component, pageProps }) {
 
