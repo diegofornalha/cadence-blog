10a9dfbc784271ec443f3f86a9a791edfd6dd4e6
Stackbit Code Editor <projects@stackbit.com>
2023-01-01 08:06:01 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,11 @@
-import Analytics from 'components/Analytics'
 
-import Document, { Head, Main, NextScript } from 'next/document'
+
+import Document, { , Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
     render() {
         return (
             <html>
-                <Head>
-                </Head>
                 <body>
                     <Main />
                     <NextScript />
