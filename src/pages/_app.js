ccb94ea37647e9d11eedc0ca5882532ea61bfb61
Stackbit Code Editor <projects@stackbit.com>
2023-01-01 06:37:31 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,19 +4,6 @@ import '../css/main.css';
 
 export default function MyApp({ Component, pageProps }) {
 
-    const router = useRouter();
-
-    useEffect(() => {
-    const handleRouteChange = (url) => {
-      pageview(url);
-    };
-
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-    }, [router.events]);
 
 
     return <Component {...pageProps} />;
