938f1b2111edc189ba8d40f2a2f5bb478f8a3074
Stackbit Code Editor <projects@stackbit.com>
2023-01-01 08:21:24 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,27 +1,5 @@
 import '../css/main.css';
-import Analytics from '../components/Analytics';
 
-export default function MyApp({ Component, pageProps }) {
-    return (
-        <Analytics>
-            <Component {...pageProps} />
-        </Analytics>
-    );
-}
-
-// Path: src\components\Analytics.js
-import { useEffect } from 'react';
-
-export default function Analytics() {
-    useEffect(() => {
-        // Inserir o código do Analytics aqui
-    }, []);
-
-    return null;
-}
-
-// Path: src\pages\_app.js
-import '../css/main.css';
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
 }
