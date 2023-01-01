3d9757ac5a7d2710bafbad1a35a99bb8a44ce799
Stackbit Code Editor <projects@stackbit.com>
2023-01-01 06:48:19 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,16 +3,16 @@ import Script from 'next/script';
 
 export default function MyApp({ Component, pageProps }) {
     <>
-    
-    <Script src={'https://www.googletagmanager.com/gtag/js?id=G-99JF8KBFVZ'}/>
-    <Script id="google-analytics" strategy="lazyOnload">
-        {`
+
+        <Script src={'https://www.googletagmanager.com/gtag/js?id=G-99JF8KBFVZ'} strategy='afterInteractive' />
+        <Script id="google-analytics" strategy="afterInteractive">
+            {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-99JF8KBFVZ');
         `}
-    </Script>
+        </Script>
     </>
 
     return <Component {...pageProps} />;
