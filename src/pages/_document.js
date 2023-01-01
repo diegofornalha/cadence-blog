9a7e6e6087536cca5f45d4a7ebbbfb300330e826
Stackbit Code Editor <projects@stackbit.com>
2023-01-01 08:04:00 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,12 +7,7 @@ export default class MyDocument extends Document {
         return (
             <html>
                 <Head>
-                    <meta charSet="utf-8" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1" />
-                    <link rel="stylesheet" href="/_next/static/style.css" />
-                    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-                    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-                </Head>
+ </Head>
                 <body>
                     <Main />
                     <NextScript />
